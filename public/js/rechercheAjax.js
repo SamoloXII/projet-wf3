0dbbcf439bb206157fb177c6abfcaea0d460c6b8
Jérémy BATILLIOT <jeremy.batilliot23@gmail.com>
2019-12-15 03:08:13 +0100

    test

---
+++ public/js/rechercheAjax.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+
+    // $('#search-medoc').keyup(function () {
+    //
+    //     var medicament = $(this).val();
+    //
+    //
+    //     if (medicament != "") {
+    //
+    //         console.log(medicament);
+    //
+    //         //
+    //     //     $.ajax({
+    //     //         type: 'GET',
+    //     //         url: ''
+    //     //         console.log(medicament);
+    //     //
+    //     //     });
+    //
+    //
+    //     }
+    //
+    // });
+
+    // var searchRequest = null;
+    //
+    // $('#search-medoc').keyup(function () {
+    //
+    //     var minLength = 1;
+    //     var that = $this;
+    //     var value = $(this).val();
+    //     var entitySelector = $("entitiesNav").html('');
+    //     if(value.length >= minLength){
+    //         if(searchRequest != null){
+    //             searchRequest.abort();
+    //             searchRequest= $.ajax({
+    //                 type: "GET",
+    //                 url: "{{ path ('')}}"
+    //             })
+    //         }
+    //     }
+    //
+    // })
+
+
+});
--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -26,16 +29,54 @@ class AccueilController extends AbstractController
     /**
      * @Route("/profil")
      */
-    public function profil(){
+    public function profil()
+    {
         return $this->render('user/profil.html.twig');
     }
 
     /**
      * @Route("/calendrier")
      */
-    public function calendrier(){
+    public function calendrier()
+    {
         return $this->render('user/calendrier.html.twig');
     }
+
+//    /**
+//     * @param Request $request
+//     * @return Response
+//     * @Route("/search")
+//     */
+//    public function searchMedoc(Request $request)
+//    {
+//        $em = $this->getDoctrine()->getManager();
+//
+//        // POST
+//        $requestString = $request->get('m');
+//
+//        $posts = $em->getRepository('App:Medicaments')->findEntitiesByString($requestString);
+//
+//        if (!$posts) {
+//
+//            $result['posts']['error'] = "Post no found :(";
+//        } else {
+//            $result['posts']=$this->getRealEntities($posts);
+//        }
+//
+//        return new Response(json_encode($result));
+//
+//    }
+//
+//    public function getRealEntities($posts)
+//    {
+//        foreach ($posts as $post){
+//            $realEntities[$post->getId()] = [$post->getNom()];
+//        }
+//
+//        return $realEntities;
+//    }
+
+
 }
 
 
